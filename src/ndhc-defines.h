da7d34a300629580dc8fa6be2af89f51e66e3902
Nicholas J. Kain <nicholas@kain.us>
2015-02-18 05:41:31 -0500

    Bump version to 2.0.
    When the coroutine restructuring is fully tested and debugged, it
    will definitely be a significant enough change for a full version
    bump.

--- src/ndhc-defines.h
+++ src/ndhc-defines.h
@@ -3,7 +3,7 @@
 
 #define PID_FILE_DEFAULT "/var/run/ndhc.pid"
 #define PID_FILE_IFCH_DEFAULT "/var/run/ifchd.pid"
-#define NDHC_VERSION "1.5"
+#define NDHC_VERSION "2.0"
 #define MAX_BUF 1024
 
 #endif /* NDHC_DEFINES_H_ */
